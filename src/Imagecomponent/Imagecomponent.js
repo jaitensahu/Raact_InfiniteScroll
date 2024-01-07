59cb7cad6a639dc7f5cad008b383326d357e097f
Jaiten <Jaitensahu9@gmail.com>
2024-01-07 23:41:18 +0530

    updated

--- src/Imagecomponent/Imagecomponent.js
+++ src/Imagecomponent/Imagecomponent.js
@@ -3,7 +3,7 @@ import './Imagecomponent.css'
 const Image=(props)=>{
    return <>
     <div className="singleImgContainer">
-        <img src={props.data.urls.full} alt="" />
+        <img src={props.data.urls.small} alt="" />
         <div className="screen"></div>
         <div className="text">{props.data.alt_description.toUpperCase()}</div>
     </div>
